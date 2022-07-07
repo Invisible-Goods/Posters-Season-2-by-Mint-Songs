b8feb9d99ef46f6e8d691baedf926a6a20f6a281
Sweets Sweetman <sweetmantech@gmail.com>
2022-07-07 15:17:15 -0300

    I modularize tests a little more.

--- contracts/test/PosterFactory.t.sol
+++ contracts/test/PosterFactory.t.sol
@@ -3,25 +3,20 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "../PosterFactory.sol";
+import "./libraries/ERC1155/ERC1155Receiver.sol";
 import "./libraries/ds-test/src/test.sol";
 
-contract PosterFactoryTest is DSTest {
-    PosterFactory greeter;
+contract PosterFactoryInitialize is DSTest, ERC1155Receiver {
+    PosterFactory poster;
+    string name = "my smart contract";
+    string symbol = "wagmi";
+    uint256 weiPerPoster = 100;
 
-    function setUp() public {}
+    function setUp() public {
+        poster = new PosterFactory();
 
-    function testInitialize() public {
-        greeter = new PosterFactory();
-
-        assertEq(greeter.weiPerPoster(), 0);
-        assertEq(greeter.name(), "");
-        assertEq(greeter.symbol(), "");
-
-        string memory name = "my smart contract";
-        string memory symbol = "wagmi";
-
-        greeter.initialize(
-            100,
+        poster.initialize(
+            weiPerPoster,
             name,
             symbol,
             "my dontract description",
@@ -30,9 +25,47 @@ contract PosterFactoryTest is DSTest {
             300,
             0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38
         );
+    }
 
-        assertEq(greeter.weiPerPoster(), 100);
-        assertEq(greeter.name(), name);
-        assertEq(greeter.symbol(), symbol);
+    function testInitialized() public {
+        assertEq(poster.weiPerPoster(), weiPerPoster);
+        assertEq(poster.name(), name);
+        assertEq(poster.symbol(), symbol);
+        assertEq(
+            poster.contractURI(),
+            "data:application/json;base64,eyJuYW1lIjogIm15IHNtYXJ0IGNvbnRyYWN0IiwgImRlc2NyaXB0aW9uIjogIm15IGRvbnRyYWN0IGRlc2NyaXB0aW9uIiwgImltYWdlIjogImlwZnM6Ly9jaWQiLCAiZXh0ZXJuYWxfbGluayI6ICJodHRwczovL21pbnRzb25ncy5jb20iLCAic2VsbGVyX2ZlZV9iYXNpc19wb2ludHMiOiAiMzAwIiwgImZlZV9yZWNpcGllbnQiOiAiMHhjZmJmMzRkMzg1ZWEyZDVlYjk0NzA2M2I2N2VhMjI2ZGNkYTNkYzM4In0="
+        );
+    }
+
+    function testCreatePoster() public {
+        poster.createPoster{value: 10000}(
+            "Sweet Poster",
+            "My Sweet Poster",
+            "ipfs://cid",
+            0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38,
+            100
+        );
+        assertEq(
+            poster.uri(1),
+            "data:application/json;base64,eyJuYW1lIjogIlN3ZWV0IFBvc3RlciIsICJkZXNjcmlwdGlvbiI6ICJNeSBTd2VldCBQb3N0ZXIiLCAidXJsIjogImlwZnM6Ly9jaWQifQ=="
+        );
+    }
+
+    function testRoyaltyInfo() public {
+        poster.createPoster{value: 10000}(
+            "Sweet Poster",
+            "My Sweet Poster",
+            "ipfs://cid",
+            0xcfBf34d385EA2d5Eb947063b67eA226dcDA3DC38,
+            100
+        );
+        assertEq(
+            poster.uri(1),
+            "data:application/json;base64,eyJuYW1lIjogIlN3ZWV0IFBvc3RlciIsICJkZXNjcmlwdGlvbiI6ICJNeSBTd2VldCBQb3N0ZXIiLCAidXJsIjogImlwZnM6Ly9jaWQifQ=="
+        );
+        assertEq(
+            poster.uri(2),
+            "data:application/json;base64,eyJuYW1lIjogIiIsICJkZXNjcmlwdGlvbiI6ICIiLCAidXJsIjogIiJ9"
+        );
     }
 }
---
+++ contracts/test/libraries/ERC1155/ERC1155Receiver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract ERC1155Receiver {
+    function onERC1155Received(
+        address operator,
+        address from,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external pure returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155Received(address,address,uint256,uint256,bytes)"
+                )
+            );
+    }
+}
